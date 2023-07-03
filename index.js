fdb204023b70ecdb0ea372ba4a66f4fe213131be
austinkelsay <austinkelsay@yahoo.com>
2023-07-03 14:20:52 -0500

    Script is working

---
+++ index.js
@@ -0,0 +1,40 @@
+function updateTabs() {
+  // Select all tabs
+  const tabs = Array.from(document.querySelectorAll('a[role="tab"]'));
+
+  // Find the 'For You' tab and remove it
+  const forYouTab = tabs.find(
+    (tab) => tab.innerText.trim().toLowerCase() === "for you"
+  );
+
+  // Check if the 'For You' tab exists before removing
+  if (forYouTab) {
+    forYouTab.parentNode.removeChild(forYouTab);
+  }
+
+  // Find the 'Following' tab, change its text, and select it
+  const followingTab = tabs.find(
+    (tab) => tab.innerText.trim().toLowerCase() === "following"
+  );
+
+  if (followingTab) {
+    // grab the child of the child of the child from this node since to get the text
+    const tabText = followingTab.childNodes[0].childNodes[0].childNodes[0];
+    // Change the text to 'Following Only'
+    tabText.innerText = "Following Only";
+
+    // Make the tab full width
+    followingTab.style.width = "100%";
+
+    followingTab.click();
+  }
+}
+
+// Initial update of tabs
+updateTabs();
+
+// Use a MutationObserver to detect changes in the page structure
+// Twitter uses a SPA structure, so we have to check when the page changes
+const observer = new MutationObserver(updateTabs);
+
+observer.observe(document.body, { childList: true, subtree: true });
